513e88abb6f3b644815f967605f11c560cf0e5dc
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2023-10-16 19:50:40 +0900

    20231016

--- ex3/src/test/java/org/zerock/ex3/Ex3ApplicationTests.java
+++ ex3/src/test/java/org/zerock/ex3/Ex3ApplicationTests.java
@@ -3,11 +3,29 @@ package org.zerock.ex3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootTest
 class Ex3ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testConnection() {
+
+		try (Connection con =
+					 DriverManager.getConnection(
+							 "jdbc:mariadb://localhost:3306/bootex?serverTimezone=Asia/Seoul",
+							 "bootuser",
+							 "1234")) {
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+
 	}
 
 }
